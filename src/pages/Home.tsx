f7f684fd0549213c3f2bc5819a4b37f560e509ec
Ramon Nobre Peres <r.peres@xsensors.ai>
2022-09-21 19:20:10 -0300

    changes in layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,22 @@
+import { useCallback, useRef } from 'react';
 import { gql, useQuery } from '@apollo/client';
-import { Container, BoxCenter, TextTitle, ContentRow, Text } from './styles';
-import backgroundImage from '../assets/background-image.jpg';
+import {
+  Container,
+  BoxCenter,
+  TextTitle,
+  ContentRow,
+  Text,
+  ContentSlider,
+  Btn,
+  SatelliteAnimation,
+} from './styles';
+import {
+  MdOutlineArrowBackIosNew,
+  MdOutlineArrowForwardIos,
+} from 'react-icons/md';
+import { Blurhash } from 'react-blurhash';
+import Slider from 'react-slick';
+import satelliteIcon from '../assets/satellite.png';
 
 const GET_DATA = gql`
   query {
@@ -17,59 +33,144 @@ const GET_DATA = gql`
 `;
 
 export function Home() {
+  const svgAnimation = Array.from(Array(20).keys());
   const { data, loading } = useQuery(GET_DATA);
+  const slider = useRef<Slider>(null);
 
   console.log({ loading, data }); //eslint-disable-line
+  console.log(svgAnimation); //eslint-disable-line
+  const settings = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    draggable: false,
+  };
+
+  //* Interaction with Slider
+  const handleNext = useCallback(() => {
+    slider?.current?.slickNext();
+  }, []);
+
+  const handlePrev = useCallback(() => {
+    slider?.current?.slickPrev();
+  }, []);
 
   return (
     <Container>
-      <img src={backgroundImage} alt="space" />
+      <SatelliteAnimation>
+        {svgAnimation.map((p) => (
+          <img key={`${p}`} src={satelliteIcon} alt="satellite" />
+        ))}
+      </SatelliteAnimation>
 
       <BoxCenter>
-        <TextTitle>Launch Latest</TextTitle>
+        <Blurhash className="blurHash" hash="L43ls]*08w8wVqV?kCkX8wMc%#.9" />
 
-        <ContentRow>
-          <Text>Mission: Starlink-15 (v1.0)</Text>
-          <Text>Date: 2020-10-24</Text>
-        </ContentRow>
+        <Btn type="button" className="prev" onClick={handlePrev}>
+          <MdOutlineArrowBackIosNew />
+        </Btn>
 
-        <ContentRow>
-          <Text>Launch Site: CCAFS SLC 40</Text>
-          <Text>Is tentative: Yes</Text>
-        </ContentRow>
+        <Slider {...settings} ref={slider} focusOnSelect={false}>
+          <ContentSlider>
+            <TextTitle>Launch Latest</TextTitle>
 
-        <Text>
-          Details: SpaceX 21st ISS resupply mission on behalf of NASA and the
-          first under the CRS-2 contract, this mission brings essential supplies
-          to the International Space Station using the cargo variant of SpaceX
-          Dragon 2 spacecraft. The external payload for this mission is the
-          Nanoracks Bishop Airlock. Falcon 9 and Dragon launch from LC-39A,
-          Kennedy Space Center and the booster is expected to land on an ASDS.
-          The mission will be complete with return and recovery of the Dragon
-          capsule and down cargo.
-        </Text>
+            <ContentRow>
+              <Text>Mission: Starlink-15 (v1.0)</Text>
+              <Text>Date: 2020-10-24</Text>
+            </ContentRow>
 
-        <ContentRow>
-          <Text>Rocket Name: Falcon 9</Text>
-          <Text>Rocket Type: FT</Text>
-        </ContentRow>
+            <ContentRow>
+              <Text>Launch Site: CCAFS SLC 40</Text>
+              <Text>Is tentative: Yes</Text>
+            </ContentRow>
 
-        <ContentRow>
-          <Text>Rocket Country: USA</Text>
-          <Text>Rocket Company: SpaceX</Text>
-        </ContentRow>
+            <Text>
+              Details: SpaceX 21st ISS resupply mission on behalf of NASA and
+              the first under the CRS-2 contract, this mission brings essential
+              supplies to the International Space Station using the cargo
+              variant of SpaceX Dragon 2 spacecraft. The external payload for
+              this mission is the Nanoracks Bishop Airlock. Falcon 9 and Dragon
+              launch from LC-39A, Kennedy Space Center and the booster is
+              expected to land on an ASDS. The mission will be complete with
+              return and recovery of the Dragon capsule and down cargo.
+            </Text>
 
-        <Text>
-          Rocket Description: Falcon 9 is a two-stage rocket designed and
-          manufactured by SpaceX for the reliable and safe transport of
-          satellites and the Dragon spacecraft into orbit.
-        </Text>
+            <ContentRow>
+              <Text>Rocket Name: Falcon 9</Text>
+              <Text>Rocket Type: FT</Text>
+            </ContentRow>
 
-        <Text>Mission Accomplished</Text>
+            <ContentRow>
+              <Text>Rocket Country: USA</Text>
+              <Text>Rocket Company: SpaceX</Text>
+            </ContentRow>
 
-        <Text>Video: https://www.youtube.com/watch?v=8riKQXChPGg</Text>
+            <Text>
+              Rocket Description: Falcon 9 is a two-stage rocket designed and
+              manufactured by SpaceX for the reliable and safe transport of
+              satellites and the Dragon spacecraft into orbit.
+            </Text>
 
-        <Text>Wikipedia: https://en.wikipedia.org/wiki/Starlink</Text>
+            <Text>Mission Accomplished</Text>
+
+            <Text>Video: https://www.youtube.com/watch?v=8riKQXChPGg</Text>
+
+            <Text>Wikipedia: https://en.wikipedia.org/wiki/Starlink</Text>
+          </ContentSlider>
+
+          <ContentSlider>
+            <TextTitle>Launch test</TextTitle>
+
+            <ContentRow>
+              <Text>Mission: Starlink-15 (v1.0)</Text>
+              <Text>Date: 2020-10-24</Text>
+            </ContentRow>
+
+            <ContentRow>
+              <Text>Launch Site: CCAFS SLC 40</Text>
+              <Text>Is tentative: Yes</Text>
+            </ContentRow>
+
+            <Text>
+              Details: SpaceX 21st ISS resupply mission on behalf of NASA and
+              the first under the CRS-2 contract, this mission brings essential
+              supplies to the International Space Station using the cargo
+              variant of SpaceX Dragon 2 spacecraft. The external payload for
+              this mission is the Nanoracks Bishop Airlock. Falcon 9 and Dragon
+              launch from LC-39A, Kennedy Space Center and the booster is
+              expected to land on an ASDS. The mission will be complete with
+              return and recovery of the Dragon capsule and down cargo.
+            </Text>
+
+            <ContentRow>
+              <Text>Rocket Name: Falcon 9</Text>
+              <Text>Rocket Type: FT</Text>
+            </ContentRow>
+
+            <ContentRow>
+              <Text>Rocket Country: USA</Text>
+              <Text>Rocket Company: SpaceX</Text>
+            </ContentRow>
+
+            <Text>
+              Rocket Description: Falcon 9 is a two-stage rocket designed and
+              manufactured by SpaceX for the reliable and safe transport of
+              satellites and the Dragon spacecraft into orbit.
+            </Text>
+
+            <Text>Mission Accomplished</Text>
+
+            <Text>Video: https://www.youtube.com/watch?v=8riKQXChPGg</Text>
+
+            <Text>Wikipedia: https://en.wikipedia.org/wiki/Starlink</Text>
+          </ContentSlider>
+        </Slider>
+
+        <Btn type="button" className="next" onClick={handleNext}>
+          <MdOutlineArrowForwardIos />
+        </Btn>
       </BoxCenter>
     </Container>
   );
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,4 +1,23 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+import background from '../assets/background-image.jpg';
+
+const satellite = keyframes`
+  0% {
+    left: -100%;
+  } 
+  100% {
+    left: 100%;
+  }
+`;
+
+const image = keyframes`
+  0% {
+    transform: rotate(0deg);
+  } 
+  100% {
+    transform: rotate(10deg);
+  }
+`;
 
 export const Container = styled.div`
   display: flex;
@@ -8,30 +27,62 @@ export const Container = styled.div`
   height: 100%;
   width: 100%;
 
-  background-image: url('../assets/background-image.jpg');
+  background-image: url(${background});
   background-size: cover;
   background-position: 50% 50%;
   background-repeat: no-repeat;
-
-  img {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    z-index: 100;
-  }
 `;
 
 export const BoxCenter = styled.div`
   z-index: 100;
+  width: 100%;
   max-width: 1150px;
-  max-height: 800px;
+  position: absolute;
+  height: 100%;
+  max-height: 700px;
   padding: 3.125rem;
   border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  gap: 1.875rem;
+  overflow: hidden;
+
   box-shadow: 0px 4px 10px 5px rgba(0, 0, 0, 0.3);
   background: ${(props) => props.theme.black};
+
+  .blurHash {
+    height: 100% !important;
+    width: 100% !important;
+    position: absolute !important;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    border-radius: 15px;
+    opacity: 0.4;
+  }
+
+  .slick-slide {
+    height: 100%;
+
+    > div {
+      height: 100%;
+    }
+  }
+
+  .slick-slider,
+  .slick-list {
+    height: 100%;
+    overflow: hidden;
+  }
+
+  .slick-list {
+    padding: 5px 0 !important;
+  }
+
+  .slick-track {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    margin: 0;
+  }
 `;
 export const TextTitle = styled.span`
   color: ${(props) => props.theme.white};
@@ -39,14 +90,73 @@ export const TextTitle = styled.span`
   align-self: center;
   font-size: 2.5rem;
   font-weight: 600;
+  z-index: 200;
 `;
 export const ContentRow = styled.div`
   display: flex;
+  z-index: 200;
 
   span {
     min-width: 25rem;
   }
 `;
 export const Text = styled.span`
+  z-index: 200;
   color: ${(props) => props.theme.white};
+`;
+export const ContentSlider = styled.div`
+  display: flex !important;
+  flex-direction: column;
+  gap: 1.875rem;
+`;
+
+export const Btn = styled.button`
+  position: absolute;
+  top: 60%;
+  z-index: 2;
+
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+
+  background: #fff;
+  transition: background-color 0.2s ease-in-out;
+
+  > svg {
+    color: red;
+    transition: color 0.2s ease-in-out;
+  }
+
+  &:hover {
+    background: #f1f1f1;
+
+    > svg {
+      color: yellow;
+    }
+  }
+
+  &.next {
+    right: 0.5rem;
+  }
+  &.prev {
+    left: 0.5rem;
+  }
+`;
+
+export const SatelliteAnimation = styled.div`
+  display: flex;
+  position: absolute;
+  bottom: 25%;
+  width: 60%;
+  height: 4%;
+  gap: 20px;
+
+  animation: ${satellite} 20s infinite;
+
+  > img {
+    animation: ${image} 3s infinite;
+    max-height: 100%;
+    max-width: 100%;
+    transform: perspective(75em) rotateX(18deg);
+    box-shadow: rgba(22, 31, 39, 0.42) 0px 60px 123px -25px,
+      rgba(19, 26, 32, 0.08) 0px 35px 75px -35px;
+  }
 `;
